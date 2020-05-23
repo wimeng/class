cc89a4b089ff8844c167769d9e7746a0bfed9b7f
wimeng <wimeng@umich.edu>
2020-05-22 22:59:00 -0400

    some refactoring

--- util.js
+++ util.js
@@ -1,3 +1,12 @@
+var ulcs = ["eecs367", "eecs373", "eecs381", "eecs388", "eecs427",
+            "eecs442", "eecs445", "eecs470", "eecs475", "eecs476",
+            "eecs477", "eecs478", "eecs481", "eecs482", "eecs483",
+            "eecs484", "eecs485", "eecs486", "eecs487", "eecs489",
+            "eecs490", "eecs491", "eecs492", "eecs493"];
+var cs_capstone = ["eecs440", "eecs441", "eecs443", "eecs467", "eecs473",
+                   "eecs480", "eecs494", "eecs495", "eecs497", "eecs470", ]
+
+
 function num_to_color(perc, darkness) {
   var dark_diff = 255 - darkness;
   var color_num = dark_diff * 65536 + darkness;
@@ -27,8 +36,15 @@ function hideshow(id) {
   }
 }
 
-
 function normalize(course) {
   var res = course.toUpperCase();
   return res.substring(0, res.length - 3) + " " + res.substring(res.length - 3, res.length);
+}
+
+function check_if(course_set, req_set) {
+  var x = "";
+  for (x of req_set) {
+    if (course_set.has(x)) { return x; }
+  }
+  return "none";
 }
