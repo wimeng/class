843bf4aa420101bdbce007b9b2b53280a68a23f0
wimeng <wimeng@umich.edu>
2020-05-23 02:50:00 -0400

    major refactoring done

---
+++ lookup.js
@@ -0,0 +1,2 @@
+// this will store a map containing course names and their credit hours.
+// if a course has a 0 as the numebr of credit hours, it means that it does not exist
--- util.js
+++ util.js
@@ -1,10 +1,41 @@
+var compsci = [["math115", "math120", "math185", false],
+               ["math116", "math121", "math186", false],
+               ["eecs203", false],
+               ["eecs280", false],
+               ["eecs281", false],
+               ["eecs370", false],
+               ["eecs376", false],
+               ["stats250", "stats265", "stats280", "stats412", "stats426", "ioe265", false],
+               ["eecs440", "eecs441", "eecs443", "eecs467", "eecs473",
+                "eecs480", "eecs494", "eecs495", "eecs497", "eecs470", true],
+               ["eecs367", "eecs373", "eecs381", "eecs388", "eecs427",
+                "eecs442", "eecs445", "eecs470", "eecs475", "eecs476",
+                "eecs477", "eecs478", "eecs481", "eecs482", "eecs483",
+                "eecs484", "eecs485", "eecs486", "eecs487", "eecs489",
+                "eecs490", "eecs491", "eecs492", "eecs493", true],
+               ["eecs367", "eecs373", "eecs381", "eecs388", "eecs427",
+                "eecs442", "eecs445", "eecs470", "eecs475", "eecs476",
+                "eecs477", "eecs478", "eecs481", "eecs482", "eecs483",
+                "eecs484", "eecs485", "eecs486", "eecs487", "eecs489",
+                "eecs490", "eecs491", "eecs492", "eecs493", true],
+               ["eecs367", "eecs373", "eecs381", "eecs388", "eecs427",
+                "eecs442", "eecs445", "eecs470", "eecs475", "eecs476",
+                "eecs477", "eecs478", "eecs481", "eecs482", "eecs483",
+                "eecs484", "eecs485", "eecs486", "eecs487", "eecs489",
+                "eecs490", "eecs491", "eecs492", "eecs493", true],
+               ["eecs367", "eecs373", "eecs381", "eecs388", "eecs427",
+                "eecs442", "eecs445", "eecs470", "eecs475", "eecs476",
+                "eecs477", "eecs478", "eecs481", "eecs482", "eecs483",
+                "eecs484", "eecs485", "eecs486", "eecs487", "eecs489",
+                "eecs490", "eecs491", "eecs492", "eecs493", true]];
+
 var ulcs = ["eecs367", "eecs373", "eecs381", "eecs388", "eecs427",
             "eecs442", "eecs445", "eecs470", "eecs475", "eecs476",
             "eecs477", "eecs478", "eecs481", "eecs482", "eecs483",
             "eecs484", "eecs485", "eecs486", "eecs487", "eecs489",
             "eecs490", "eecs491", "eecs492", "eecs493"];
 var cs_capstone = ["eecs440", "eecs441", "eecs443", "eecs467", "eecs473",
-                   "eecs480", "eecs494", "eecs495", "eecs497", "eecs470", ]
+                   "eecs480", "eecs494", "eecs495", "eecs497", "eecs470"];
 
 
 function num_to_color(perc, darkness) {
@@ -42,9 +73,31 @@ function normalize(course) {
 }
 
 function check_if(course_set, req_set) {
-  var x = "";
-  for (x of req_set) {
-    if (course_set.has(x)) { return x; }
+  for (var i = 0; i < req_set.length - 1; i++) {
+    if (course_set.has(req_set[i])) { return req_set[i]; }
   }
   return "none";
+}
+
+function item_done(item_label, item, show) {
+  var temp_desc = document.getElementById(item_label).innerHTML;
+  document.getElementById(item_label).innerHTML = document.getElementById(item_label).innerHTML.substring(0,1) + "O" + document.getElementById(item_label).innerHTML.substring(2);
+  if (show) {
+    document.getElementById(item_label).innerHTML = document.getElementById(item_label).innerHTML + ": " + normalize(item);
+  }
+  return true;
+}
+
+function check_major(course_set, major, list_num) {
+  var count = 0;
+  var temp_course;
+  var temp = new Set();
+  for (var i = 0; i < major.length; i++) {
+    temp_course = check_if(course_set, major[i]);
+    if (temp_course != "none") { item_done("m" + list_num + "item" + i, temp_course, major[i][major[i].length - 1]); count++; temp.add(temp_course); course_set.delete(temp_course); }
+  }
+  course_set = new Set([...course_set, ...temp]);
+  document.getElementById("percentage" + list_num).innerHTML = count + "/" + major.length + " classes taken (" + (count / major.length * 100).toFixed(3) + "%)";
+  document.getElementById("major" + list_num).style.backgroundColor = num_to_color(count / major.length, 40);
+  return true;
 }
